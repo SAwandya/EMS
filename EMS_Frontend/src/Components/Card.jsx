db2b58527a8c0ab8f5f62f383bbae9e8f5f010ad
SAwandya <sachilaawandya@gmail.com>
2024-09-16 18:45:42 +0530

    create reusable card components

--- EMS_Frontend/src/Components/Card.jsx
+++ EMS_Frontend/src/Components/Card.jsx
@@ -1,6 +1,12 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import BadgeTwoToneIcon from "@mui/icons-material/BadgeTwoTone";
+import useEmployees from "../hooks/useEmployees";
 
-const Card = () => {
+
+const Card = ({children, title, number}) => {
+
+  const { data } = useEmployees();
+
   return (
     <Box
       sx={{
@@ -8,8 +14,26 @@ const Card = () => {
         width: "60vh",
         backgroundColor: "#F0EEFA",
         borderRadius: "30px",
+        display: "flex",
       }}
-    ></Box>
+    >
+      {children}
+      <Box sx={{ marginTop: "19px" }}>
+        <Typography
+          sx={{
+            fontWeight: "bold",
+            fontSize: "20px",
+            marginLeft: "5px",
+            color: "#7f8c9b",
+          }}
+        >
+          {title}
+        </Typography>
+        <Typography variant="h3" sx={{ marginTop: "7px", color: "#444b54" }}>
+          {number}
+        </Typography>
+      </Box>
+    </Box>
   );
 }
 
--- EMS_Frontend/src/Components/Content.jsx
+++ EMS_Frontend/src/Components/Content.jsx
@@ -2,9 +2,17 @@ import React from "react";
 import Card from "./Card";
 import { Box, Typography } from "@mui/material";
 import { useRenderContent } from "../Context/RenderContentContext";
+import BadgeTwoToneIcon from "@mui/icons-material/BadgeTwoTone";
+import useEmployees from "../hooks/useEmployees";
+import AccountBalanceIcon from "@mui/icons-material/AccountBalance";
+import useDepartments from "../hooks/useDepartments";
 
 const Content = () => {
   const renderContent = useRenderContent();
+
+  const { data: employeeData } = useEmployees();
+
+  const { data: departmentData } = useDepartments();
 
   return (
     <Box component="main" sx={{ flexGrow: 1, p: 0 }}>
@@ -18,8 +26,19 @@ const Content = () => {
           justifyContent: "space-between",
         }}
       >
-        <Card />
-        <Card />
+        <Card title="Total number of employees" number={employeeData?.length}>
+          <BadgeTwoToneIcon
+            sx={{ fontSize: "70px", color: "#7350F5", margin: "20px" }}
+          />
+        </Card>
+        <Card
+          title="Total number of Departments"
+          number={departmentData?.length}
+        >
+          <AccountBalanceIcon
+            sx={{ fontSize: "70px", color: "#7350F5", margin: "20px" }}
+          />
+        </Card>
       </Box>
       {renderContent()}
     </Box>
